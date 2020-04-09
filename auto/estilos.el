5fd3138d955cd50d247af7002fd2d839841f53e0
Ariel Serranoni <arielserranoni@gmail.com>
2020-04-09 03:01:56 -0300

    nadica''

--- auto/estilos.el
+++ auto/estilos.el
@@ -4,15 +4,16 @@
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("babelbib" "fixlanguage")))
    (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
    (TeX-run-style-hooks
     "common"
     "csquotes"
@@ -25,6 +26,7 @@
     "mathrsfs"
     "cancel"
     "tikz-cd"
+    "bm"
     "imakeidx"
     "mathastext"
     "type1cm"
@@ -49,6 +51,7 @@
     "lema"
     "exercicio"
     "notacao"
-    "sublema"))
+    "sublema"
+    "postulado"))
  :latex)
 
